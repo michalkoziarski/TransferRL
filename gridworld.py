ce3db399b5f294c4d3c98580a2b4ce824ec191cb
Michał Koziarski <michalkoziarski@gmail.com>
2016-02-10 20:05:53 +0100

    gridworld

--- gridworld.py
+++ gridworld.py
@@ -5,10 +5,26 @@ Direction = Enum('Direction', 'none left right up down')
 
 
 class GridWorld:
+    penalty = 0.01
+
     def __init__(self, grid=None, width=10, height=10, entities=None, display=False):
-        pass
+        if grid:
+            self.grid = grid
+        else:
+            self.grid = GridWorld.generate(entities, width, height)
+
+        for x in range(len(self.grid)):
+            for y in range(len(self.grid[0])):
+                self.grid[x][y] = self.grid[x][y](self.grid, x, y)
+
+        self.penalty = GridWorld.penalty
+        self.display = display
 
     def state(self):
+        pass
+
+    @staticmethod
+    def generate(entities, width, height):
         pass
 
 
@@ -25,16 +41,21 @@ class CounterMetaClass(type):
 class Entity:
     __metaclass__ = CounterMetaClass
 
+    color = '#000000'
+
     def __init__(self, grid, x, y):
         self.grid = grid
         self.x = x
         self.y = y
+        self.color = self.__class__.color
 
     def id(self):
         return self.id
 
 
 class Agent(Entity):
+    color = '#B873FF'
+
     def move(self, direction):
         pass
 
@@ -42,39 +63,50 @@ class Agent(Entity):
 class Block(Entity):
     reward = 0
     terminal = False
-    color = '#000000'
 
     def __init__(self, grid, x, y):
         super(Block, self).__init__(grid, x, y)
 
         self.reward = self.__class__.reward
         self.terminal = self.__class__.terminal
-        self.color = self.__class__.color
 
     def interact(self, block):
         pass
 
 
+class Empty(Block):
+    color = '#D4D1CA'
+
+
 class Goal(Block):
-    pass
+    reward = 1
+    terminal = True
+    color = '#FFD225'
 
 
 class Water(Block):
-    pass
+    reward = -0.1
+    color = '#0A73FF'
+
+
+class Fire(Block):
+    reward = -1
+    terminal = True
+    color = '#B20702'
 
 
 class Wall(Block):
-    pass
+    color = '#422C25'
 
 
 class Portal(Block):
-    pass
+    color = '#481B5E'
 
 
 class Switch(Block):
-    pass
+    color = '#218B87'
 
 
 class Door(Block):
-    pass
+    color = '#B63F00'
 
