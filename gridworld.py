eda5d60032fbf245e579f99e8b30fd1e352cf49f
Michał Koziarski <michalkoziarski@gmail.com>
2016-02-10 20:35:18 +0100

    display

--- gridworld.py
+++ gridworld.py
@@ -1,3 +1,5 @@
+import pygame
+
 from enum import Enum
 
 
@@ -7,18 +9,20 @@ Direction = Enum('Direction', 'none left right up down')
 class GridWorld:
     penalty = 0.01
 
-    def __init__(self, grid=None, width=10, height=10, entities=None, display=False):
+    def __init__(self, grid=None, width=10, height=10, entities=None):
         if grid:
             self.grid = grid
         else:
             self.grid = GridWorld.generate(entities, width, height)
 
-        for x in range(len(self.grid)):
-            for y in range(len(self.grid[0])):
-                self.grid[x][y] = self.grid[x][y](self.grid, x, y)
+        self.width = len(self.grid)
+        self.height = len(self.grid[0])
+
+        for x in range(self.width):
+            for y in range(self.height):
+                self.grid[x][y] = self.grid[x][y](self, x, y)
 
         self.penalty = GridWorld.penalty
-        self.display = display
 
     def state(self):
         pass
@@ -43,8 +47,8 @@ class Entity:
 
     color = '#000000'
 
-    def __init__(self, grid, x, y):
-        self.grid = grid
+    def __init__(self, grid_world, x, y):
+        self.grid_world = grid_world
         self.x = x
         self.y = y
         self.color = self.__class__.color
@@ -110,3 +114,27 @@ class Switch(Block):
 class Door(Block):
     color = '#B63F00'
 
+
+class Display:
+    def __init__(self, width=10, height=10, field_size=30):
+        pygame.init()
+
+        self.width = width
+        self.height = height
+        self.field_size = field_size
+        self.screen = pygame.display.set_mode((self.width * field_size, self.height * field_size))
+
+    def xy2rect(self, x, y):
+        return pygame.Rect(x * self.field_size + 1, y * self.field_size + 1, self.field_size - 2, self.field_size - 2)
+
+    def xy2color(self, grid, x, y):
+        return pygame.Color(self.colors[grid.get(x, y)])
+
+    def draw(self, grid):
+        self.screen.fill((255, 255, 255))
+
+        for x in range(self.width):
+            for y in range(self.height):
+                pygame.draw.rect(self.screen, grid[x][y].color, self.xy2rect(x, y))
+
+        pygame.display.update()
