5b7ac3b5a8b58259a2f26900c52d42f8dd11459c
Michał Koziarski <michalkoziarski@gmail.com>
2016-02-22 19:17:35 +0100

    logging reward every episode

--- trainer.py
+++ trainer.py
@@ -128,11 +128,11 @@ while params['episode'] < params['episodes']:
     print 'Episode #%d: total reward of %.2f in %d steps, with exploration rate %.2f' % \
           (params['episode'], gw.total_reward(), gw.t(), params['exploration_rate'])
 
+    with open(os.path.join('models', model_name, 'log.csv'), 'a') as f:
+        f.write('%d,%.2f,%d\n' % (params['episode'], gw.total_reward(), gw.t()))
+
     if params['episode'] % params['save_step'] == 0:
         print 'Saving model...'
-
-        with open(os.path.join('models', model_name, 'log.csv'), 'a') as f:
-            f.write('%d,%.2f,%d\n' % (params['episode'], gw.total_reward(), gw.t()))
 
         with open(os.path.join('models', model_name, 'params.json'), 'w') as f:
             json.dump(params, f, indent=2, separators=(',', ': '))
