dfaffa08ab98ed4dc8c8bb3ba1246cac1439c0dd
Michał Koziarski <michalkoziarski@gmail.com>
2016-06-06 19:11:51 +0200

    running experiments via bash scripts

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ worlds=("simple" "coin" "water_fire" "coin_water_fire" "portal" "door" "door_por
 
 for i in {1..3}; do
     for world in ${worlds[@]}; do
-        model_name="{$world}_{$i}"
-        world_path="world_{$world}.json"
+        model_name="${world}_${i}"
+        world_path="world_${world}.json"
 
         sbatch batch.sh $model_name $world_path
     done
